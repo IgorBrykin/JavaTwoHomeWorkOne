cb00eafe1a12218f152e2866c405a7241693b1d1
IgorBrykin <igorbrikin@gmail.com>
2020-05-16 11:10:24 +0300

    Cat

--- src/participants/Cat.java
+++ src/participants/Cat.java
@@ -1,4 +1,14 @@
 package participants;
 
-public class Cat {
+public class Cat implements Skills{
+
+    @Override
+    public void jump() {
+        System.out.println("Кот прыгнул");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Кот пробежал");
+    }
 }
