0d5679c8b4c6230e8ab2052e750a4d554f933f5c
IgorBrykin <igorbrikin@gmail.com>
2020-05-19 14:14:56 +0300

    Hello, its commit HW2

--- src/WeekEnum/Main.java
+++
@@ -1,8 +0,0 @@
-package WeekEnum;
-
-public class Main {
-    public static void main(String[] args) {
-
-
-    }
-}
--- src/WeekEnum/WeeK.java
+++
@@ -1,6 +0,0 @@
-package WeekEnum;
-
-public enum WeeK {
-    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY,SATURDAY, SUNDAY;
-
-}
